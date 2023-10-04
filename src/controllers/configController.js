76c3c1361cfc1f7875c55a72442b9d0076d81d45
Nhatdev94 <nhatdev94@gmail.com>
2023-10-04 08:31:35 +0700

    update be

--- src/controllers/configController.js
+++ src/controllers/configController.js
@@ -1,5 +1,13 @@
 import db from '../db.js'
 
+const formatStringToNumber = (string) => {
+    if (typeof string !== 'string') return string
+    let result = string
+    result = result.replace(/,/g, '')
+    result = Number(result)
+    return result
+}
+
 const configController = {
     getCategory: (req, res) => {
         db.query(`SELECT * FROM Categorys`, (err, rows) => {
@@ -13,7 +21,7 @@ const configController = {
         })
     },
     createCategoryItem: (req, res) => {
-        db.query(`INSERT INTO Categorys (name, amount_limit) VALUES ('${req.body.name}', '${req.body.amount_limit || 0}')`, (err, rows) => {
+        db.query(`INSERT INTO Categorys (name, amount_limit) VALUES ('${req.body.name}', '${formatStringToNumber(req.body.amount_limit) || 0}')`, (err, rows) => {
             if (err) {
                 console.log('Create Category Item Fail', err);
                 return false
@@ -24,7 +32,7 @@ const configController = {
         })
     },
     updateCategoryItem: (req, res) => {
-        db.query(`Update Categorys SET name = '${req.body.name}', amount_limit = '${req.body.amount_limit}' WHERE category_id = '${req.body.category_id}'`, (err, rows) => {
+        db.query(`Update Categorys SET name = '${req.body.name}', amount_limit = '${formatStringToNumber(req.body.amount_limit)}' WHERE category_id = '${formatStringToNumber(req.body.category_id)}'`, (err, rows) => {
             if (err) {
                 console.log('Update Category Item Fail', err);
                 return false
